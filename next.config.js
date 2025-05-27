4b34e98cae9eeb6de75ae0b1438f6a4594a22640
Mineaworld <mineawebdev@gmail.com>
2025-05-27 10:17:55 +0700

    Remove experimental font loader configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -5,14 +5,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  experimental: {
-    fontLoaders: [
-      {
-        loader: '@next/font/google',
-        options: { timeout: 15000 }, // Increase timeout to 15 seconds
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
